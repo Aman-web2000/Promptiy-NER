cabdea33d045ad39eb14ef1a09166bf2b0ad4ea1
Aman Chauhan <66059307+Aman-web2000@users.noreply.github.com>
2024-09-06 11:16:28 +0530

    config commit

---
+++ config.py
@@ -0,0 +1,7 @@
+api_key="sk-VL9D3nRO4466t93p4kYZPfRrvBr-2ar5tLeA78NAeTT3BlbkFJLRVSnFCeAWjnsQde1S0o_WWN6THqGesgVOZRlU9cYA"
+nlp_model='ner.jinja'
+labels=["Account", "Industry","Company","Organization","CORPORATION", "Sector", "Sub-Sector","Portfolio","Offering","Market Offering","Position", "Role"]
+domain="English"
+description=""
+one_shot_training_data=""
+one_shot_labelled_training_data=""
