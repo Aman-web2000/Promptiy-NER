692eba4a8baa95b9a4ef8645b91c649b0f519d2b
Aman Chauhan <66059307+Aman-web2000@users.noreply.github.com>
2024-09-09 14:34:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-from promptify import Prompter, OpenAI
+from promptify import Prompter, OpenAI, Pipeline
 from config import api_key, labels, nlp_model, domain,one_shot_labelled_training_data, description
 
 model = OpenAI(api_key)
-nlp_prompter = Prompter(model)
+prompter = Prompter(nlp_model)
+pipeline = Pipeline(prompter , model)
 
 ## Zero Shot Learning 
 def ner_zero_shot(text):
-    result= nlp_prompter.fit(nlp_model, domain= domain, text_imput=text, labels=labels)
-    
-    return eval(result['text'])
+    result= pipeline.fit(text_input=text,domain= domain, labels=None)
+
+    return result[0]['text']
+
+## Zero shot with Custom Tags
+def _ner_zero_shot_tags(text):
+    result = pipeline.fit(text_input= text, domain=domain, labels=labels)
+
+    return result
 
 ## One Shot Learning
 def ner_one_shot(text):
-    result = nlp_prompter.fit(nlp_model,
+    result = pipeline.fit( text_input  = text,
                           domain      = domain,
-                          text_input  = text,
+                          examples    = one_shot_labelled_training_data,
+                          labels      = labels)
+    
+    return result[0]['text']
+
+## One Shot Learning
+def ner_one_shot_desc(text):
+    result = pipeline.fit( text_input  = text,
+                          domain      = domain,
                           examples    = one_shot_labelled_training_data,
                           description = description,
                           labels      = labels)
     
-    return eval(result['text'])
+    return result[0]['text']
