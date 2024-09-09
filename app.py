61ea0debf4e19c5c6d0d9e45b16f6ad93961affe
Aman Chauhan <66059307+Aman-web2000@users.noreply.github.com>
2024-09-09 14:34:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from main import ner_zero_shot, ner_one_shot
+from main import ner_zero_shot, _ner_zero_shot_tags,ner_one_shot, ner_one_shot_desc
 
 st.title("NER : Promptify")
 
@@ -7,14 +7,17 @@ st.write("Named Entity Recognition using Promptify")
 
 text=st.text_input("Enter the Text", key="Text Box")
 
-mode = st.sradio("Mode of NER (Zero/One)", ['Zero Shot','One Shot'])
+mode = st.sradio("Mode of NER (Zero/One)", ['Zero Shot','Zero shot with custom Label','One Shot','One shot with domian knowledge'])
 
 if st.button("RUN"):
 
-    if mode=="Zero Shot":
+    if "Zero Shot" in mode:
+        if "custom" in mode:
+            output= _ner_zero_shot_tags(text)
         output= ner_zero_shot(text)
     else:
+        if "domain" in mode:
+            output= ner_one_shot_desc(text)
         output= ner_one_shot(text)
 
     st.write(output)
-    
